63d9eb10e58918e31604fc2e410203ca3f7455af
Eustaquio Rangel <taq@eustaquiorangel.com>
2017-07-23 13:32:32 -0300

    Módulos "inception".

---
+++ modulos/inception1.rb
@@ -0,0 +1,12 @@
+module Inception
+  def self.hello
+    puts 'hello'
+  end
+
+  def self.world
+    puts 'world'
+  end
+end
+
+Inception.hello
+Inception.world
---
+++ modulos/inception2.rb
@@ -0,0 +1,15 @@
+module Inception
+  extend self
+
+  def hello
+    puts 'hello'
+  end
+
+  def world
+    puts 'world'
+  end
+end
+
+Inception.hello
+Inception.world
+
