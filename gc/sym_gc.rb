a7258dfbab600f62e41294a6134fcdf7cb9d27a2
Eustaquio Rangel <taq@eustaquiorangel.com>
2023-02-24 15:13:17 -0300

    Minitest reporters

--- gc/sym_gc.rb
+++ gc/sym_gc.rb
@@ -1,11 +1,10 @@
-before =  Symbol.all_symbols.size
+puts Symbol.all_symbols.size
 
 100_000.times do |i|
   "sym#{i}".to_sym
 end
 
 puts Symbol.all_symbols.size
-
 GC.start
 
 puts Symbol.all_symbols.size
