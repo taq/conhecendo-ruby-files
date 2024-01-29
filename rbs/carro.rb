6e5947e58aa6c84984ea5616dedc93133ab2f909
Eustaquio Rangel <taq@eustaquiorangel.com>
2024-01-29 15:51:19 -0300

    Mostrando o typeprof rodando sem exemplo de código

--- rbs/carro.rb
+++ rbs/carro.rb
@@ -1,14 +1,12 @@
 class Carro
   attr_reader :marca, :modelo, :tanque
   attr_accessor :cor
-  @@qtde = 0
 
   def initialize(marca, modelo, cor, tanque)
     @marca  = marca
     @modelo = modelo
     @cor    = cor
     @tanque = tanque
-    @@qtde += 1
   end
 end
 
