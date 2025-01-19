6f86cec4a7a112e3861a11500aa55357fd4e6ac4
Eustaquio Rangel <taq@eustaquiorangel.com>
2025-01-19 20:40:31 -0300

    Servidor TCP com echo

---
+++ io/tcpserver3.rb
@@ -0,0 +1,21 @@
+require "socket"
+
+TCPServer.open("localhost", 8081) do |server|
+  puts "servidor iniciado"
+
+  loop do
+    puts "aguardando conexão ..."
+    con = server.accept
+    puts "conexão recebida!"
+    con.puts "Sua conexão foi recebida!"
+
+    while true
+      str = con.gets
+      break if str.match?(/sair/)
+
+      puts "você digitou: #{str}"
+    end
+
+    con.close
+  end
+end
