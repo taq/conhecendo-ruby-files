5ccf9530aa4cb2f1cf0be96b9b6d84ee6cecd46d
Eustaquio Rangel <taq@eustaquiorangel.com>
2025-08-22 17:59:12 -0300

    Ops, faltaram esses arquivos

--- basico/exc4.rb
+++ basico/exc4.rb
@@ -2,8 +2,8 @@ begin
   numero = 1
   string = "oi"
   numero + string
-rescue StandardError => exception
-  puts "Ocorreu um erro do tipo #{exception.class}: #{exception}"
+rescue StandardError => e
+  puts "Ocorreu um erro do tipo #{e.class}: #{e}"
   puts msg
 ensure
   puts "Lascou tudo."
--- basico/retry.rb
+++ basico/retry.rb
@@ -3,8 +3,8 @@ numero2 = "dois"
 
 begin
   puts numero1 + numero2
-rescue StandardError => exception
-  puts "Ops, problemas aqui (#{exception.class}), vou tentar de novo."
+rescue StandardError => e
+  puts "Ops, problemas aqui (#{e.class}), vou tentar de novo."
   numero2 = 2
   retry
 end
