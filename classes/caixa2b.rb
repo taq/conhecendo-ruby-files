4d3cb5fcecabcc2f76f989bb95cf54474d2dedbe
Eustaquio Rangel <taq@eustaquiorangel.com>
2025-10-18 12:38:07 -0300

    Mostrando o problema de acesso

---
+++ classes/caixa2b.rb
@@ -0,0 +1,29 @@
+class CaixaDeParafusos
+  attr_accessor :quantidade
+
+  def initialize(quantidade)
+    @quantidade = quantidade
+  end
+
+  def to_s
+    "Quantidade de parafusos na caixa #{self.object_id}: #{@quantidade}"
+  end
+
+  def +(outra)
+    CaixaDeParafusos.new(@quantidade + outra.quantidade)
+    @quantidade = 0
+    outra.quantidade = 0
+  end
+end
+
+caixa1 = CaixaDeParafusos.new(10)
+caixa2 = CaixaDeParafusos.new(20)
+caixa3 = caixa1 + caixa2
+
+puts caixa1
+puts caixa2
+puts caixa3
+
+# Isso não deveria estar acessível em escopo público ...
+caixa1.quantidade = 10
+puts caixa1
