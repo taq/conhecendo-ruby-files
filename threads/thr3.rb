45781593084054cedb09b43e71abe50f8fce24be
Eustaquio Rangel <taq@eustaquiorangel.com>
2025-11-16 13:51:15 -0300

    Melhorando o exemplo inicial de threads rodando

--- threads/thr3.rb
+++ threads/thr3.rb
@@ -1,14 +1,18 @@
 proc = Proc.new do |parametro|
+  puts "Loop até #{parametro}"
+
   parametro.times do |valor|
-    print "[#{valor + 1}/#{parametro}]"
+    print "[em #{Thread.current.object_id}, #{valor + 1} de #{parametro}]"
     sleep 0.5
   end
 end
 
-thread = nil
+threads = []
+
 5.times do |valor|
-  thread = Thread.new(valor, &proc)
+  puts "Chamando com #{valor}"
+  threads << Thread.new(valor, &proc)
 end
 
-thread.join
-puts "Terminado!"
+threads.map(&:join)
+puts "\n\nTerminado!"
