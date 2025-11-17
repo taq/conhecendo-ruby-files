417568ffe3389a6429a70a8d36c2fd505df3de00
Eustaquio Rangel <taq@eustaquiorangel.com>
2025-11-17 13:57:24 -0300

    Exemplos da Async

--- threads/async.rb
+++ threads/async.rb
@@ -1,25 +1,24 @@
 # typed: false
 # frozen_string_literal: true
+
 require 'json'
 require 'net/http'
 require 'async'
 
 start = Time.now
 
-Thread.new do
-  Fiber.set_scheduler Async::Scheduler.new
-
+Async do
   %w[Joao Maria Eustaquio].each do |name|
-    Fiber.schedule do
-      current      = Time.now
-      uri          = URI.parse("https://api.agify.io/")
-      http         = Net::HTTP.new(uri.host, uri.port)
+    Async do
+      current = Time.now
+      uri = URI.parse("https://api.agify.io/")
+      http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
-      json         = JSON.parse(http.get("/?name=#{name}").body)
-      age          = json['age']
+      json = JSON.parse(http.get("/?name=#{name}").body)
+      age = json['age']
       puts "%s: parece ter %s anos (levei %.3f segundos para calcular)\n" % [name, age, Time.now - current]
     end
   end
-end.join
+end
 
 puts "Tempo total: #{Time.now - start}"
