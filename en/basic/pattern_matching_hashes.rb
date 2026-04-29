1167b02cc17a11246a50700f0ade3ada54c462e3
Eustaquio Rangel <taq@eustaquiorangel.com>
2026-04-29 09:26:11 -0300

    Fixed keys to english

--- en/basic/pattern_matching_hashes.rb
+++ en/basic/pattern_matching_hashes.rb
@@ -1,10 +1,10 @@
-translations = { original: :br, texto_original: 'Bom dia', destino: :en, texto_traduzido: 'Good morning' }
+translations = { original: :br, original_text: 'Bom dia', destino: :en, translated_text: 'Good morning' }
 
 case translations
-in { original: :br, texto_original: texto_original, destino: :es, texto_traduzido: texto_traduzido }
-  puts "'#{texto_original}' in Spanish is '#{texto_traduzido}'"
-in { original: :br, texto_original: texto_original, destino: :en, texto_traduzido: texto_traduzido }
-  puts "'#{texto_original}' in English is '#{texto_traduzido}'"
+in { original: :br, original_text: original_text, destino: :es, translated_text: translated_text }
+  puts "'#{original_text}' in Spanish is '#{translated_text}'"
+in { original: :br, original_text: original_text, destino: :en, translated_text: translated_text }
+  puts "'#{original_text}' in English is '#{translated_text}'"
 else
   puts "No translation"
 end
